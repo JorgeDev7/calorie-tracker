49f974ec74f6e5f2dbb0ac615b45cf6566dad09a
JorgeDev <jorgehumbertocolindres@gmail.com>
2024-04-02 12:42:42 -0600

    State 'activity' created to manage every field on the form Element.
    HTML generated to complete the UI, every input or element inside the Form Element has each 'value' and 'onChange' event.
    
    'validateInputText' implemented with 'useMemo' to validate which category is selected to change between two values in the Input Submit.
    
    'handleChangeSelect' func to set values to the State and parsing 'category' and 'calories' to a number.
    
    'isValidActivity' validates if 'name' and 'calories' fields are empty.
    
    'handleSubmit' waiting to implement his functionality.

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,5 +1,103 @@
+import { useState, ChangeEvent, useMemo, FormEvent } from "react"
+import { categories } from "../data/categories"
+
 export default function Form() {
+
+    const [activity, setActivity] = useState<Activity>({
+        category: 1,
+        name: '',
+        calories: 0
+    });
+
+    const validateInputText = useMemo(() => {
+        return activity.category === 1 ? 'Guardar Comida' : 'Guardar Ejercicio'
+    }, [activity.category])
+
+    const handleChangeSelect = (e: ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>) => {
+        const isNumberField = ['category', 'calories'].includes(e.target.id);
+
+        setActivity({
+            ...activity,
+            [e.target.id]: isNumberField ? +e.target.value : e.target.value
+        })
+    }
+
+    const isValidActivity = () => {
+        const { name, calories } = activity;
+
+        return name.trim() !== "" && calories > 0;
+    }
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        //Todo: save activity
+    }
+
     return (
-        <div>Form</div>
+        <form
+            className="space-y-5 bg-white shadow p-10 rounded-lg"
+            onSubmit={handleSubmit}
+        >
+            <div className="grid grid-cols-1 gap-3">
+                <label
+                    htmlFor="category"
+                    className="font-bold"
+                >Categoría:</label>
+                <select
+                    name="category"
+                    id="category"
+                    className="border boder-slate-300 p-2 rounded-lg w-full bg-white"
+                    value={activity.category}
+                    onChange={handleChangeSelect}
+                >
+                    {categories.map(categorie => (
+                        <option
+                            key={categorie.id}
+                            value={categorie.id}
+                        >{categorie.name}</option>
+                    ))}
+                </select>
+            </div>
+
+            <div className="grid grid-cols-1 gap-3">
+                <label
+                    htmlFor="name"
+                    className="font-bold"
+                >Actividad:</label>
+                <input
+                    type="text"
+                    name="name"
+                    id="name"
+                    className="border border-slate-300 p-2 rounded-lg"
+                    placeholder="Ej. Comida, Jugo de Naranja, Ensalada, Ejercicio, Pesas, Bicicleta"
+                    value={activity.name}
+                    onChange={handleChangeSelect}
+                />
+            </div>
+
+            <div className="grid grid-cols-1 gap-3">
+                <label
+                    htmlFor="calories"
+                    className="font-bold"
+                >Calorías:</label>
+                <input
+                    type="number"
+                    name="calories"
+                    id="calories"
+                    className="border border-slate-300 p-2 rounded-lg"
+                    placeholder="Calorías ej. 300 o 500"
+                    value={activity.calories}
+                    onChange={handleChangeSelect}
+                    min={0}
+                />
+            </div>
+
+            <input
+                type="submit"
+                className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer disabled:opacity-10 disabled:cursor-not-allowed"
+                value={validateInputText}
+                disabled={!isValidActivity() ? true : false}
+            />
+        </form>
     )
 }
